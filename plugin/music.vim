c624f5742174a1a60308746e1ce1a55fd7c2aa35
pablo <26plau26@gmail.com>
2020-11-17 08:29:08 -0500

    added spotitfy support (experimental)

--- plugin/music.vim
+++ plugin/music.vim
@@ -1,24 +1,66 @@
 " :redraw is called because vim crashes after a silent execute
-function Song()
-  silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.py song"
-  silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.scpt"
-  :redraw!
-  :echom "Vim Music Message: Playing Song"
-endfunction
 
-function Playlist()
-  silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.py playlist"
-  silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.scpt"
-  :redraw!
-  :echom "Vim Music Message: Running Playlist"
-endfunction
+let g:spotify = 0
 
-function Stop()
-  silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.py stop"
-  silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.scpt"
-  :redraw!
-  :echom "Vim Music Message: Stop Song"
-endfunction
+if spotify == 0
+  function Song()
+    silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.py song"
+    silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.scpt"
+    :redraw!
+    :echom "Vim Music Message: Playing Song"
+  endfunction
+
+  function Playlist()
+    silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.py playlist"
+    silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.scpt"
+    :redraw!
+    :echom "Vim Music Message: Running Playlist"
+  endfunction
+
+  function Stop()
+    silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.py stop"
+    silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.scpt"
+    :redraw!
+    :echom "Vim Music Message: Stop Song"
+  endfunction
+
+elseif spotify == 1
+  function Song()
+    silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.py ssong"
+    silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.scpt"
+    :redraw!
+    :echom "Vim Music Message: Playing Song"
+  endfunction
+
+  function Playlist()
+    silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.py splaylist"
+    silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.scpt"
+    :redraw!
+    :echom "Vim Music Message: Running Playlist"
+  endfunction
+
+  function Stop()
+    silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.py sstop"
+    silent execute "!~/.vim/plugged/vim-music/scripts/vim-music.scpt"
+    :redraw!
+    :echom "Vim Music Message: Stop Song"
+  endfunction
+
+else
+  " error checking
+  function Song()
+    echo "Vim Music Message: Invalid option for global variable spotify, must be 1 or 0."
+  endfunction
+
+  function Playlist()
+    echo "Vim Music Message: Invalid option for global variable spotify, must be 1 or 0."
+  endfunction
+
+  function Stop()
+    echo "Vim Music Message: Invalid option for global variable spotify, must be 1 or 0."
+  endfunction
+
+endif
 
 :command Playlist :call Playlist()
 :command PlaySong :call Song()
--- scripts/vim-music.py
+++ scripts/vim-music.py
@@ -4,33 +4,67 @@ from pathlib import Path
 
 home_dir = str(Path.home())
 
-if sys.argv[1] == "song":
-  name = input("Enter the song name: ")
+if __name__ == "__main__":
+  # apple music
+  if sys.argv[1] == "song":
+    name = input("Enter the song name: ")
 
-  lines = ["#!/usr/bin/env osascript\n",
-           'tell application "Music"\n', 
-           f'  play the track named "{name}"\n', 
-           "end tell\n"]
+    lines = ["#!/usr/bin/env osascript\n",
+             'tell application "Music"\n', 
+             f'  play the track named "{name}"\n', 
+             "end tell\n"]
 
-  with open(f"{home_dir}/.vim/plugged/vim-music/scripts/vim-music.scpt", "w") as v:
-    v.writelines(lines)
+    with open(f"{home_dir}/.vim/plugged/vim-music/scripts/vim-music.scpt", "w") as v:
+      v.writelines(lines)
 
-elif sys.argv[1] == "playlist":
-  name = input("Enter the playlist name: ")
+  elif sys.argv[1] == "playlist":
+    name = input("Enter the playlist name: ")
 
-  lines = ["#!/usr/bin/env osascript\n",
-           'tell application "Music"\n', 
-           f'  play the playlist named "{name}"\n', 
-           "end tell\n"]
+    lines = ["#!/usr/bin/env osascript\n",
+             'tell application "Music"\n', 
+             f'  play the playlist named "{name}"\n', 
+             "end tell\n"]
 
-  with open(f"{home_dir}/.vim/plugged/vim-music/scripts/vim-music.scpt", "w") as v:
-    v.writelines(lines)
+    with open(f"{home_dir}/.vim/plugged/vim-music/scripts/vim-music.scpt", "w") as v:
+      v.writelines(lines)
 
-elif sys.argv[1] == "stop":
-  lines = ["#!/usr/bin/env osascript\n",
-           'tell application "Music"\n', 
-           '  pause\n',
-           "end tell\n"]
+  elif sys.argv[1] == "stop":
+    lines = ["#!/usr/bin/env osascript\n",
+             'tell application "Music"\n', 
+             '  pause\n',
+             "end tell\n"]
 
-  with open(f"{home_dir}/.vim/plugged/vim-music/scripts/vim-music.scpt", "w") as v:
-    v.writelines(lines)
+    with open(f"{home_dir}/.vim/plugged/vim-music/scripts/vim-music.scpt", "w") as v:
+      v.writelines(lines)
+
+  # Spotify
+  elif sys.argv[1] == "ssong":
+    name = input("Enter the song name: ")
+
+    lines = ["#!/usr/bin/env osascript\n",
+             'tell application "Spotify"\n', 
+             f'  play the track named "{name}"\n', 
+             "end tell\n"]
+
+    with open(f"{home_dir}/.vim/plugged/vim-music/scripts/vim-music.scpt", "w") as v:
+      v.writelines(lines)
+
+  elif sys.argv[1] == "splaylist":
+    name = input("Enter the playlist name: ")
+
+    lines = ["#!/usr/bin/env osascript\n",
+             'tell application "Spotify"\n', 
+             f'  play the playlist named "{name}"\n', 
+             "end tell\n"]
+
+    with open(f"{home_dir}/.vim/plugged/vim-music/scripts/vim-music.scpt", "w") as v:
+      v.writelines(lines)
+
+  elif sys.argv[1] == "sstop":
+    lines = ["#!/usr/bin/env osascript\n",
+             'tell application "Spotify"\n', 
+             '  pause\n',
+             "end tell\n"]
+
+    with open(f"{home_dir}/.vim/plugged/vim-music/scripts/vim-music.scpt", "w") as v:
+      v.writelines(lines)
